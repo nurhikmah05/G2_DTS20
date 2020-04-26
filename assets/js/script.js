da4b73d90c3e29910b11ba7b933e2778ca065f4e
Adauclyaa <muhammad.syam17155@student.unsika.ac.id>
2020-04-27 00:08:03 +0700

    finishing

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+
+
+var typed = new Typed(".typedDari", {
+	strings: [
+		"Launch your Dreams Website in Just 48 hours.",
+		"Grow faster and succeed with Us.",
+		"Make it beautiful. Make your own business.",
+		"Sell your business with your Website.",
+	],
+	typeSpeed: 40,
+	startDelay: 90,
+	loop: true,
+});
---
+++ assets/js/team.js
@@ -0,0 +1,28 @@
+function detail(clicked_id){
+    const img = document.getElementById("img");
+    const name = document.getElementById("name");
+    const asal = document.getElementById("asal");
+    if (clicked_id == 5){
+        img.innerHTML =  `<img class="team2" src="./assets/img/kadri.jpg" alt="">`;
+        name.innerHTML =  `Pak Kadri`;
+        asal.innerHTML = `Banda Aceh`;
+    }else if(clicked_id == 4){
+        img.innerHTML =  `<img class="team2" src="./assets/img/IMG_0337.jpg" alt="">`;
+        name.innerHTML =  `Muhibbur`;
+        asal.innerHTML = `Surabaya`;
+    }else if(clicked_id == 3){
+        img.innerHTML =  `<img class="team2" src="./assets/img/hikmahh.jpg" alt="">`;
+        name.innerHTML =  `Nurhikmah`;
+        asal.innerHTML = `Makassar`;
+    }else if(clicked_id == 2){
+        img.innerHTML =  `<img class="team2" src="./assets/img/syam.jpg" alt="">`;
+        name.innerHTML =  `Muhammad Syam Firdaus`;
+        asal.innerHTML = `Purwakarta`;
+    }
+    else if(clicked_id == 1){
+        img.innerHTML =  `<img class="team2" src="./assets/img/mis.jpg" alt="">`;
+        name.innerHTML =  `Nidya`;
+        asal.innerHTML = `-`;
+    }
+}
+
